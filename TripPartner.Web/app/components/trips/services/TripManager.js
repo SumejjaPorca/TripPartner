e5538c50a2cfc16cdaa92a83d4e526604db93bf3
SumejjaPorca <sumejja93@gmail.com>
2015-08-19 14:22:59 +0200

    trip manager

--- TripPartner.Web/app/components/trips/services/TripManager.js
+++ TripPartner.Web/app/components/trips/services/TripManager.js
@@ -1,1 +1,8 @@
-﻿
+﻿(function (module) {
+
+    module.service('TripManager', ['Trip', '$http', 'serverName', '$q', function (Trip, $http, serverName, $q) {
+
+
+    }]);
+
+})(angular.module('app.stories'));
